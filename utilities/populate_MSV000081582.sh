1175d81a97e3b64a1f3f2fb6e9c196b921c74cef
Mingxun Wang <mwang87@gmail.com>
2018-05-24 11:00:18 -0700

    adding other metadata

--- utilities/populate_MSV000081582.sh
+++
@@ -1,1 +0,0 @@
-python ./populate.py MSV000081582 ./data/MSV000081582_metadata.csv
---
+++ utilities/populate_all.sh
@@ -0,0 +1,3 @@
+python ./populate.py MSV000081582 ./data/MSV000081582_metadata.csv
+python ./populate.py MSV000078622 ./data/MSV000078556_MSV000078622_metadata.csv
+python ./populate.py MSV000078556 ./data/MSV000078556_MSV000078622_metadata.csv
