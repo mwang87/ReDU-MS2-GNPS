e2a542bd95d1113766e81fe00d72d697f22e97c7
Mingxun Wang <mwang87@gmail.com>
2019-01-04 14:07:48 -0800

    removing files

--- utilities/populate_all.sh
+++
@@ -1,5 +0,0 @@
-python ./populate.py data/SkinMetaanalysis.tsv
-
-#python ./populate.py MSV000081582 ./data/MSV000081582_metadata.csv
-#python ./populate.py MSV000078622 ./data/MSV000078556_MSV000078622_metadata.csv
-#python ./populate.py MSV000078556 ./data/MSV000078556_MSV000078622_metadata.csv
