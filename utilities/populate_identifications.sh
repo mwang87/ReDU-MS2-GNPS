eb47abf1cf98ba90ac369dc8d8ce09e9435dc460
Mingxun Wang <mwang87@gmail.com>
2019-01-04 16:19:14 -0800

    updating script to be one click

--- utilities/populate_identifications.sh
+++ utilities/populate_identifications.sh
@@ -1,2 +1,7 @@
-#python ./populate_identifications.py data/explorer/VIEWLIBRARY-698fc5a0-molecule_explorer_v2_filedetails-main.tsv
-python ./populate_identifications_libsearch.py data/identifications/MOLECULAR-LIBRARYSEARCH-V2-f2fb72f0-download_all_identifications-main.tsv
+#This script syncs down the identifications for all files and then shoves it into the database
+
+
+ID_FILENAME=all_identifications.tsv
+wget "http://gnps.ucsd.edu/ProteoSAFe/DownloadResultFile?task=058564829a434277a5899f92fe4825a9&block=main&file=DB_result/" --output-document $ID_FILENAME
+python ./populate_identifications_libsearch.py $ID_FILENAME
+rm $ID_FILENAME
