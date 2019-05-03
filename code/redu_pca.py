1cf5aa6c8f3f491ec2ec8a3448fa5ca3754308e8
cacevesucsd <44014693+cacevesucsd@users.noreply.github.com>
2019-05-03 16:15:16 -0700

    Update redu_pca.py

--- code/redu_pca.py
+++ code/redu_pca.py
@@ -4,9 +4,9 @@ import pandas as pd
 import sys
 import os
 import csv
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import cm
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import seaborn
 
 PATH_TO_COMPONENT_MATRIX = "./component_matrix.csv" #Eigenvectors
 PATH_TO_ORIGINAL_PCA = "./original_pca.csv" #original PCA matrix of the original files
@@ -15,48 +15,54 @@ PATH_TO_ORIGINAL_PCA = "./original_pca.csv" #original PCA matrix of the original
 ### Given a file input occurrence table, creates the eigen vectors file defined above PATH_TO_COMPONENT_MATRIX, and PCA project of these files PATH_TO_ORIGINAL_PCA
 def calculate_master_projection(input_file_occurrences_table):
     with open(input_file_occurrences_table, 'r') as csv_file:
-        reader = csv.DictReader(csv_file, delimiter = "\t") 
+        reader = csv.reader(csv_file, delimiter = "\t") 
         file_list = next(reader)
     useable_cols = len(file_list)
+    
+    compound_location = file_list.index("LibraryID") #finding the proper column for compound list
 
-    df = pd.read_csv(input_file_occurrences_table, sep = "\t", skip_blank_lines = True)
-    compound_list = list(df["LibraryID"])
+    if compound_location == 0:
+        col_range = np.arange(2, useable_cols)
+    else:
+        col_range = np.arange(0, useable_cols-2)
 
-    df = pd.read_csv(input_file_occurrences_table, sep = "\t", usecols = range(2, useable_cols), header = 0, skip_blank_lines = True)
 
-    new_matrix = df.values.transpose() #align so that the "features" are column headers
+    compound_list = np.loadtxt(input_file_occurrences_table, dtype = str, delimiter = "\t", usecols = compound_location, skiprows = 1)
+    df = pd.read_csv(input_file_occurrences_table, sep = "\t", usecols = col_range, header = 0, skip_blank_lines = True)
+
+    new_matrix = df.values.T #align so that the "features" are column headers
 
     pca = PCA(n_components = 5) #creating the instance
     pca.fit(new_matrix) #fitting the data 
 
     component_matrix = pca.components_ #principle components / vectors
-    dataframe1 = pd.DataFrame(data = component_matrix, columns = compound_list)
-    dataframe1 = dataframe1.T
-    dataframe1.index.name = 'LibraryID'
-    dataframe1.to_csv(PATH_TO_COMPONENT_MATRIX)
-
+    dataframe1 = pd.DataFrame(data = component_matrix.transpose())
+    dataframe1['filename'] = compound_list
+    dataframe1.to_csv(r"component_matrix.csv")
+    
     sklearn_output = pca.transform(new_matrix) #using sklearn to calculate the output
-    dataframe4 = pd.DataFrame(data = sklearn_output)
-    dataframe4.index.name = "filename"
-    dataframe4.to_csv(PATH_TO_ORIGINAL_PCA)
-
+    
+    dataframe2 = pd.DataFrame(data = sklearn_output)
+    dataframe2.to_csv(r"original_pca.csv")
 
 ### Given a new file occurrence table, creates a projection of the new data along with the old data and saves as a png output
 def project_new_data(input_file_occurrences_table, output_png):
+    new_matrix = np.array([]) 
+    file_list = []
+
     component_matrix = pd.read_csv(PATH_TO_COMPONENT_MATRIX, sep = ",")
+    old_compound_list = list(component_matrix['filename']) #list of all compounds found in the OG data
 
-    old_compound_list = list(component_matrix['LibraryID']) #list of all compounds found in the OG data
-
-    component_matrix = component_matrix.drop(['LibraryID'], axis = 1)
+    component_matrix = component_matrix.drop(['Unnamed: 0', 'filename'], axis = 1) 
     component_matrix = component_matrix.transpose()
     component_matrix.columns = old_compound_list
-   
+   #the point at which I realized I'm a messy coder and you have a boatload of patience
    #figuring out the filenames for the new data
-    with open(PATH_TO_ORIGINAL_PCA, 'r') as csv_file:
-        reader = csv.reader(csv_file, delimiter = ",") 
+    with open(input_file_occurrences_table, 'r') as csv_file:
+        reader = csv.reader(csv_file, delimiter = "\t")  
         file_list = next(reader)
  
-        compound_location = file_list.index("filename")
+        compound_location = file_list.index("LibraryID") #finding the proper column for compound list
         useable_cols = len(file_list)
 
         if compound_location == 0:
@@ -74,59 +80,28 @@ def project_new_data(input_file_occurrences_table, output_png):
     df.columns = new_compound_list
     df = df[new_filtered]
     
+    
     we_dont_care, final_matrix = component_matrix.align(df, join = 'left', axis = 1, fill_value = 0)
 
     final_matrix = final_matrix.values
     mean_matrix = mean(final_matrix.T, axis = 1)
 
     C = final_matrix - mean_matrix
+
     
     component_matrix = component_matrix.values.T
     we_dont_care = we_dont_care.T
       
     visualize_stuff = C.dot(we_dont_care) #manually calculating the output for projection
-
     
+    dataframe4 = pd.read_csv(PATH_TO_ORIGINAL_PCA, sep = ",")
     dataframe3 = pd.DataFrame(data = visualize_stuff)
-    dataframe3.to_csv("new_pca_vis.csv")
     
-    omg_im_nearly_done(dataframe3, PATH_TO_ORIGINAL_PCA)
-
-def whos_that_file(file):
-    dont_care, delimiter = os.path.splitext(file)
+    scatterplot_new = seaborn.scatterplot(0, 1, data = dataframe3, marker = 'x') 
+    scatterplot_original = seaborn.scatterplot('0','1', data = dataframe4)
+    figure = scatterplot_new.get_figure()
+    figure.savefig(output_png)
     
-    if delimiter == ".tsv":
-        type = "\t"
-    elif delimiter == ".csv":
-        type = ","
-    else:
-        print("Wrong file type try again!")
-        sys.exit()
-    
-    return(type)
-
-def omg_im_nearly_done(new_pca_stuff, old_pca_stuff):
-    typec = whos_that_file(old_pca_stuff)
-    
-    principal_components_new = new_pca_stuff
-    principal_components_old = pd.read_csv("pca.csv", delimiter = typec)
-    principal_components_old = principal_components_old.drop(['Unnamed: 0'], axis = 1)
-    
-    component1 = 0
-    component2 = 1
-
-    xnew = list(principal_components_new[component1])
-    ynew = list(principal_components_new[component2])
-    
-    xold = list(principal_components_old['%s' %(component1)])
-    yold = list(principal_components_old['%s' %(component2)])
-   
-    
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111)
-
-    ax1.scatter(xnew, ynew, s=10, c='b', marker="s", label='first')
-    ax1.scatter(xold,yold, s=10, c='r', marker="o", label='second')
 
 def main():
     input_global_file_occurrences_table = sys.argv[1]
@@ -137,4 +112,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-      
