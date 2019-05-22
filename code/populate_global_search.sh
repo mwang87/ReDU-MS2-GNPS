102237fff2ed9981747403053167be1d6232d77f
Mingxun Wang <mwang87@gmail.com>
2019-05-22 16:53:44 -0700

    adding code to populate global search

---
+++ code/populate_global_search.sh
@@ -0,0 +1,2 @@
+#This script syncs down the compound occurrences for all files and then shoves it into the database
+python3 ./search_all_data.py
---
+++ code/search_all_data.py
@@ -0,0 +1,31 @@
+import sys
+#sys.path.insert(0, "..")
+from collections import defaultdict
+import json
+import pandas as pd
+import util
+import credentials
+from models import *
+
+def main():
+    all_filenames = list(Filename.select())
+
+    task_id_list = []
+
+    PARALLISM = 10
+    for i in range(PARALLISM):
+        partition_filenames = all_filenames[i::PARALLISM]
+        
+        filenames_list = [filename.filepath for filename in partition_filenames]
+
+        taskid = util.launch_GNPS_librarysearchworkflow(filenames_list, "ReDU-MS2 Global Analysis Populate %i of %i" % (i, PARALLISM), \
+            credentials.USERNAME, credentials.PASSWORD, "miw023@ucsd.edu")
+
+        task_id_list.append(taskid)
+
+    df = pd.DataFrame()
+    df["taskid"] = task_id_list
+    df.to_csv("./data/global_tasks.tsv", sep="\t", index=False)
+
+if __name__ == '__main__':
+    main()
