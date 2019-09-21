94b7865a85551315d753868d5b10deb7dd69c3df
Mingxun Wang <mwang87@gmail.com>
2019-09-20 18:07:51 -0700

    adding access logging

--- code/run_production_server.sh
+++ code/run_production_server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-gunicorn -w 4 -b 0.0.0.0:5001 --timeout 3600 main:app
+#export FLASK_ENV=development
+#python3 ./main.py
+
+gunicorn -w 4 -b 0.0.0.0:5001 --timeout 3600 main:app --access-logfile /app/logs/access.log
