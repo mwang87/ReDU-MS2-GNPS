248a0a79a85d4adbaa3d6d27bd8b234135113d19
Mingxun Wang <mwang87@gmail.com>
2020-03-13 18:26:29 -0700

    fixing test

--- test/test_validator.py
+++ test/test_validator.py
@@ -4,7 +4,7 @@ sys.path.insert(0, "../code/")
 import metadata_validator
 
 def test_valid():
-    valid_files = glob.glob("data/validator/valid*")
+    valid_files = glob.glob("data/validator/valid_*")
     for valid_filename in valid_files:
         print(valid_filename)
         passes_validation, failures, errors_list, valid_rows, row_count = metadata_validator.perform_validation(valid_filename)
@@ -14,8 +14,17 @@ def test_valid():
         assert(matched_dataset_items == len(valid_rows))
 
 def test_invalid():
-    invalid_files = glob.glob("data/validator/invalid*")
+    invalid_files = glob.glob("data/validator/invalid_*")
     for invalid_filename in invalid_files:
         print(invalid_filename)
         passes_validation, failures, errors_list, valid_rows, row_count = metadata_validator.perform_validation(invalid_filename)
         assert(passes_validation is False)
+
+def test_invalid_massive():
+    invalid_files = glob.glob("data/validator/invaliddata_*")
+    for invalid_filename in invalid_files:
+        print(invalid_filename)
+        passes_validation, failures, errors_list, valid_rows, row_count = metadata_validator.perform_validation(invalid_filename)
+        assert(passes_validation is True)
+        dataset_success, result_string, matched_dataset_items = metadata_validator.perform_validation_against_massive(invalid_filename)
+        assert(matched_dataset_items == 0)
