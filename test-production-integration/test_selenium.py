6895f11d0a5b1fb002b22646b27a3403a0c30057
cmaceves <christineaceves22@gmail.com>
2020-04-04 16:38:00 -0700

    updating selenium testing

--- test-production-integration/test_selenium.py
+++ test-production-integration/test_selenium.py
@@ -8,25 +8,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 import unittest, time, re
 import os
-
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import TimeoutException
-
+import chromedriver_binary
 SERVER_URL = os.environ.get("SERVER_URL", "https://redu.ucsd.edu")
 
 class TestInterfaceready(unittest.TestCase):
-    def setUp(self):
-        try:
-            import chromedriver_binary
-        except:
-            os.system('pip install chromedriver-binary')            
-            import chromedriver_binary
-
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        self.driver = webdriver.Chrome(options=options)
-        #self.driver = webdriver.PhantomJS()
+    def setUp(self):    
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=1420,1080')
+        chrome_options.add_argument('--headless') 
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(30)
-        #self.vars = {}
         
     def tearDown(self):
         self.driver.quit()
@@ -36,26 +30,16 @@ class TestInterfaceready(unittest.TestCase):
         #url = "https://redu.ucsd.edu/compoundenrichmentdashboard?compound=ESCITALOPRAM%20OXALATE"
         url = "{}/compoundenrichmentdashboard?compound=ESCITALOPRAM%20OXALATE".format(SERVER_URL)
         #url = "http://localhost:5006/compoundenrichmentdashboard?compound=ESCITALOPRAM%20OXALATE"  
-        print(url)
+        
         self.driver.get(url)
         time.sleep(1)
- 
-        wait = WebDriverWait(self.driver, 180)
-        print("begin")
-            
-        #waiting for the modal to go away
+        wait = WebDriverWait(self.driver, 180) 
+        #aiting for the modal to go away
         wait.until(EC.invisibility_of_element_located((By.ID, 'loadMe'))) #works when we say "visibility" locally
-        print("past check one")
-
-        #waiting for the button to be clickable
         wait.until(EC.visibility_of_element_located((By.ID,'querycompound')))
-
-        print("past check two")
         #clicking the button
         python_button = self.driver.find_element_by_id('querycompound')
-        print("past check three")
         python_button.click()
-        #time.sleep(60)
 
         try:
             alert = self.driver.switch_to.alert
