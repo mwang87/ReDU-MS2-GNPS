bbbb9c83f3ab8ba3dab41d2d8a0f134991ae09b5
Ming Wang <mwang87@gmail.com>
2020-04-06 09:56:58 -0700

    Selenium Test Update (#188)
    * Selenium testing with Chrome
    * Testing production and dev instances
    
    Co-authored-by: cmaceves <44014693+cmaceves@users.noreply.github.com>
    Co-authored-by: cmaceves <christineaceves22@gmail.com>

--- test-production-integration/test_selenium.py
+++ test-production-integration/test_selenium.py
@@ -3,31 +3,41 @@ import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 import unittest, time, re
 import os
-
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import TimeoutException
+import chromedriver_binary
 SERVER_URL = os.environ.get("SERVER_URL", "https://redu.ucsd.edu")
 
 class TestInterfaceready(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.PhantomJS()
+    def setUp(self):    
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=1420,1080')
+        chrome_options.add_argument('--headless') 
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(30)
-        self.vars = {}
-
+        
     def tearDown(self):
         self.driver.quit()
 
     def test_compound_enrichment(self):
-        #self.driver.get("{}/compoundenrichmentdashboard?compound=ESCITALOPRAM%20OXALATE".format(SERVER_URL))
-        self.driver.get("http://localhost:5005/compoundenrichmentdashboard?compound=ESCITALOPRAM%20OXALATE")
+        #going to the page
+        url = "{}/compoundenrichmentdashboard?compound=ESCITALOPRAM%20OXALATE".format(SERVER_URL)
+        
+        self.driver.get(url)
         time.sleep(1)
-        WebDriverWait(self.driver, 180).until(expected_conditions.element_to_be_clickable((By.ID, "queryresultsbutton")))
-        python_button = self.driver.find_element_by_id("queryresultsbutton")
+        wait = WebDriverWait(self.driver, 180) 
+        #aiting for the modal to go away
+        wait.until(EC.invisibility_of_element_located((By.ID, 'loadMe'))) #works when we say "visibility" locally
+        wait.until(EC.visibility_of_element_located((By.ID,'querycompound')))
+        #clicking the button
+        python_button = self.driver.find_element_by_id('querycompound')
         python_button.click()
-        #time.sleep(60)
 
         try:
             alert = self.driver.switch_to.alert
