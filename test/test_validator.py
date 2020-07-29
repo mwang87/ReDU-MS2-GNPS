a437e701322087d404e79bcc6dae226b86a93a76
Mingxun Wang <mwang87@gmail.com>
2020-07-29 09:40:44 -0700

    updating testing

--- test/test_validator.py
+++ test/test_validator.py
@@ -6,10 +6,9 @@ import metadata_validator
 def test_valid():
     valid_files = glob.glob("data/validator/valid_*")
     for valid_filename in valid_files:
-        print(valid_filename)
         passes_validation, failures, errors_list, valid_rows, row_count = metadata_validator.perform_validation(valid_filename)
         dataset_success, result_string, matched_dataset_items = metadata_validator.perform_validation_against_massive(valid_filename)
-        print(len(valid_rows), row_count, dataset_success, matched_dataset_items)
+        print("VALIDATION", valid_filename, "VALID ROWS", len(valid_rows), "TOTAL ROWS", row_count, dataset_success, "TOTAL MASSIVE VALID ROWS", matched_dataset_items)
         assert(passes_validation is True)
         assert(matched_dataset_items == len(valid_rows))
 
