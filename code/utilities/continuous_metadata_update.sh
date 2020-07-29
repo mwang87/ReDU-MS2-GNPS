0bc8a6f903d3e8adb32779307f0f12437f4ec66a
Mingxun Wang <mwang87@gmail.com>
2020-07-29 14:16:02 -0700

    adding more logging

--- code/utilities/continuous_metadata_update.sh
+++ code/utilities/continuous_metadata_update.sh
@@ -4,7 +4,7 @@ while true; do
     python3 ./populate_database.py \
     --importmetadata all \
     --importidentifications /app/database/global_tasks.tsv \
-    --identifications_output /app/database/all_identifications.tsv
+    --identifications_output /app/database/all_identifications.tsv >> /app/database/populate_metadata.log
     
     # Actually dumping everything into a file
     python3 ./dump_all_metadata.py
