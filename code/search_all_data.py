04cfc0fbf80ed9e907dfd3ef672314bff7b88aa2
cmaceves <caceves@ucsd.edu>
2021-03-03 13:33:31 -0800

    adding code to use metabatch dump not sql

--- code/search_all_data.py
+++ code/search_all_data.py
@@ -7,6 +7,22 @@ import util
 import credentials
 from models import *
 
+
+def parse_metabatch_dump():
+    metabatch_filename = '../database/metabatchdump.tsv'
+    all_filenames = pd.read_table(metabatch_filename)
+    
+    parallelism = len(all_filenames)
+
+    for index, row in all_filenames.iterrows():
+        filenames = row.filename
+        id = row.id
+        taskid = util.launch_GNPS_librarysearchworkflow(filenames, "ReDU-MS2 Global Analysis Populate %d of %d" % (id, parallelism), \
+        credentials.USERNAME, credentials.PASSWORD, "christineaceves22@gmail.com")
+        print(taskid)
+        break
+
+
 def main():
     all_filenames = list(Filename.select())
 
@@ -17,7 +33,7 @@ def main():
         partition_filenames = all_filenames[i::PARALLISM]
         
         filenames_list = [filename.filepath for filename in partition_filenames]
-
+        
         print("Searching %d Files", len(filenames_list))
 
         taskid = util.launch_GNPS_librarysearchworkflow(filenames_list, "ReDU-MS2 Global Analysis Populate %d of %d" % (i, PARALLISM), \
@@ -32,4 +48,4 @@ def main():
     df.to_csv("./database/global_tasks.tsv", sep="\t", index=False)
 
 if __name__ == '__main__':
-    main()
+    parse_metabatch_dump()
