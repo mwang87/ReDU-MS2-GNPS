a01e6b016eb80fced00db3703c74548d809199a3
cmaceves <caceves@ucsd.edu>
2021-03-04 08:16:06 -0800

    outputting task ids to df

--- code/search_all_data.py
+++ code/search_all_data.py
@@ -11,17 +11,23 @@ from models import *
 def parse_metabatch_dump():
     metabatch_filename = '../database/metabatchdump.tsv'
     all_filenames = pd.read_table(metabatch_filename)
-    
+   
+    task_id_list = []
     parallelism = len(all_filenames)
 
     for index, row in all_filenames.iterrows():
         filenames = row.filename
         id = row.id
+        taskid = id
         taskid = util.launch_GNPS_librarysearchworkflow(filenames, "ReDU-MS2 Global Analysis Populate %d of %d" % (id, parallelism), \
         credentials.USERNAME, credentials.PASSWORD, "christineaceves22@gmail.com")
-        print(taskid)
-        break
-
+        #print(taskid)
+        #break
+        task_id_list.append(taskid)
+    
+    df = pd.DataFrame()
+    df["taskid"] = task_id_list
+    df.to_csv("../database/global_tasks.tsv", sep="\t", index=False)
 
 def main():
     all_filenames = list(Filename.select())
