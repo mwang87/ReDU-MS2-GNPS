8015c9c8102047ea77a803998af4e4177ec2651f
cmaceves <caceves@ucsd.edu>
2021-03-05 13:11:42 -0800

    adding tasks cleaning up search code

--- code/search_all_data.py
+++ code/search_all_data.py
@@ -18,40 +18,15 @@ def parse_metabatch_dump():
     for index, row in all_filenames.iterrows():
         filenames = row.filename
         id = row.id
-        taskid = id
+        
         taskid = util.launch_GNPS_librarysearchworkflow(filenames, "ReDU-MS2 Global Analysis Populate %d of %d" % (id, parallelism), \
         credentials.USERNAME, credentials.PASSWORD, "christineaceves22@gmail.com")
-        #print(taskid)
-        #break
+       
         task_id_list.append(taskid)
     
     df = pd.DataFrame()
     df["taskid"] = task_id_list
     df.to_csv("../database/global_tasks.tsv", sep="\t", index=False)
-
-def main():
-    all_filenames = list(Filename.select())
-
-    task_id_list = []
-
-    PARALLISM = 20
-    for i in range(PARALLISM):
-        partition_filenames = all_filenames[i::PARALLISM]
-        
-        filenames_list = [filename.filepath for filename in partition_filenames]
-        
-        print("Searching %d Files", len(filenames_list))
-
-        taskid = util.launch_GNPS_librarysearchworkflow(filenames_list, "ReDU-MS2 Global Analysis Populate %d of %d" % (i, PARALLISM), \
-            credentials.USERNAME, credentials.PASSWORD, "miw023@ucsd.edu")
-
-        print(taskid)
-
-        task_id_list.append(taskid)
-
-    df = pd.DataFrame()
-    df["taskid"] = task_id_list
-    df.to_csv("./database/global_tasks.tsv", sep="\t", index=False)
 
 if __name__ == '__main__':
     parse_metabatch_dump()
