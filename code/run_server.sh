e4f1074384d8efb9a2adfaa0d7a49cc1623f031d
Mingxun Wang <mwang87@gmail.com>
2021-10-01 16:49:12 -0700

    updating integration testing

--- code/run_server.sh
+++ code/run_server.sh
@@ -2,7 +2,7 @@
 
 source activate py3
 
-export FLASK_ENV=development
-python3 ./main.py
+#export FLASK_ENV=development
+#python3 ./main.py
 
 gunicorn -w 2 -b 0.0.0.0:5001 --timeout 3600 main:app --access-logfile /app/logs/access.log
